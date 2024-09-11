5178e820ba60f0fa36971f38496863db58f48eb3
Missramsha <132540460+Missramsha@users.noreply.github.com>
2024-09-11 17:06:06 +0530

    New Code Changes in Team Section and Service Section

--- FsSoftWebsite2/Content/TechEaseIndia/assets/vendor/bootstrap/js/bootstrap.bundle.js
+++ FsSoftWebsite2/Content/TechEaseIndia/assets/vendor/bootstrap/js/bootstrap.bundle.js
@@ -3634,7 +3634,7 @@
       // If this is a touch-enabled device we add extra
       // empty mouseover listeners to the body's immediate children;
       // only needed because of broken event delegation on iOS
-      // https://www.quirksmode.org/blog/archives/2014/02/mouse_event_bub.html
+      // https://www.quirksmode.org/blog/archives/2014/02/mouse_event_bub
       if ('ontouchstart' in document.documentElement && !this._parent.closest(SELECTOR_NAVBAR_NAV)) {
         for (const element of [].concat(...document.body.children)) {
           EventHandler.on(element, 'mouseover', noop);
